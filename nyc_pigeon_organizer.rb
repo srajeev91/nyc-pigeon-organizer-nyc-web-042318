dd0a02e84b0033cefa2b7463f1d8b31336ad0a52
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-17 23:04:32 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  data.each do |attributes, hashes|
+    hashes.each do |info, names_array|
+      
+      names_array.each do |name|
+        
+        if names_array.include?(name)
+            if hash[name] == nil
+              hash[name] = {}
+            end
+          
+          if hash[name][attributes] == nil
+            hash[name][attributes] = []
+          end
+          hash[name][attributes].push(info.to_s)
+        end
+       
+      end
+    end
+  end
+  hash  
 end
